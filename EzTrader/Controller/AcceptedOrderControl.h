ff3ccb0d9c31b3a41aecf1cb7cd504b3aebbcb3c
Shiwan Sung <pieangel@naver.com>
2023-04-06 21:40:13 +0900

    1. 주문 컨트롤 수정.

--- EzTrader/Controller/AcceptedOrderControl.h
+++ EzTrader/Controller/AcceptedOrderControl.h
@@ -11,6 +11,11 @@ namespace DarkHorse {
 		// key : order no, value : accepted order object.
 		std::map<std::string, AcceptedOrder> order_map;
 	};
+	/// <summary>
+	/// 메인계좌 일 때는 메인 계좌와 서브 계좌의 모든 주문을 저장해야 한다.
+	/// 펀드 일 때는 펀드에 속한 모든 계좌의 주문을 저장해야 한다. 
+	/// 컨트롤에 각 계좌의 id 맵을 가지고 들어오는 주문을 필터링 해야 한다. 
+	/// </summary>
 	class AcceptedOrderControl
 	{
 	public:
