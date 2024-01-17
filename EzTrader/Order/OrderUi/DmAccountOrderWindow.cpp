af7ac5a977e5304d87aef7f60f60e565ae7259b2
pieangel@naver.com <pieangel@naver.com>
2024-01-17 14:53:24 +0900

    update DmAccountOrderWindow.cpp

--- EzTrader/Order/OrderUi/DmAccountOrderWindow.cpp
+++ EzTrader/Order/OrderUi/DmAccountOrderWindow.cpp
@@ -499,7 +499,7 @@ void DmAccountOrderWindow::recalChildWndPos()
     win_info_->children_[i]->rc_new.bottom = fixed_child_wnd_y_pos + child_wnd_height;
     it->second->set_child_wnd_pos(it->second->get_width(), child_wnd_height);
     child_wnd_xpos += it->second->get_width();
-    LOGINFO(CMyLogger::getInstance(), "child_wnd_pos[%d]", child_wnd_xpos);
+    LOGINFO(CMyLogger::getInstance(), "child_wnd_xpos[%d]", child_wnd_xpos);
 	  i++;
   }
     
